05ee96a7180e12f572673f2f3fd8a651af54e8eb
vlad <vladislav.96@mail.ru>
2020-12-01 21:50:01 +0200

    Project architecture

---
+++ src/main/java/ru/job4j/grabber/Store.java
@@ -0,0 +1,11 @@
+package ru.job4j.grabber;
+
+import ru.job4j.grabber.quartz.model.Post;
+
+import java.util.List;
+
+public interface Store {
+    void save(Post post);
+
+    List<Post> getAll();
+}
